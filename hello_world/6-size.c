0eebac31d8fd1340002f6491afc9b99d788fb976
User <user@Users-MacBook-Pro.local>
2024-02-19 15:18:20 +0100

    6 size

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+.C
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte\n", sizeof((char));
+	printf("Size of an int: %d byte(s)\n",sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);i
+} 
