4416315a242f641d9180b6ebef33a0ccd7f039ee
User <user@Users-MacBook-Pro.local>
2024-02-21 14:30:40 +0100

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,11 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - Print _putchar
- * Return: Always 0
+ * main - print agus
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
