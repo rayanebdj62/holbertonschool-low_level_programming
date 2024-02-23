8c3282dafa25c75cc937d1f5e1d6de9db5326ee6
User <user@Users-MacBook-Pro.local>
2024-02-23 09:40:55 +0100

    9time

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,42 +1,44 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * times_table - prints table from 0 to 9
+ * jack_bauer - prints sign of number
  *
- * Return: always 0 (success)
+ * Return: 0
  */
+
 void times_table(void)
 {
-int n;
-	int m;
-	int firstdigit;
-	int seconddigit;
-	int r;
-for (n = 0; n < 10; n++)
-{
-	for (m = 0; m < 10; m++)
-{
-		r = n * m;
-		firstdigit = r / 10;
-	    seconddigit = r % 10;
-			if (r <= 9 && m != 0)
-{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-}
-			if (r > 9 && m != 0)
-{
-				_putchar(44);
-				_putchar(32);
-}
-			if (r > 9)
-{
-				_putchar(firstdigit + 48);
-}
-				_putchar(seconddigit + 48);
-}
+	int i = 0;
+	int j = 0;
+	int b = 0;
 
-		_putchar('\n');
-}
+	for (i=0; i < 10; i++)
+	{
+		j = 0;
+		for (j=0; j < 10; j++)
+		{
+			b = i * j;
+		}
+
+		if (i == 0)
+		{
+			putchar ('0' + b);
+		}
+
+		else if ( b <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + b);
+		}
+		else if ( b > 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar('0' + (b / 10));
+			_putchar('0' + (b % 10));
+		}
+	}
+	_putchar ('\n');
 }
