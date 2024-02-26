9cf11abae53f86a3a1ba75a4fac455cd706a9cf0
User <user@Users-MacBook-Pro.local>
2024-02-26 13:10:31 +0100

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
