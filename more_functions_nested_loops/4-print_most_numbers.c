e4001cfdec000c1189ffc79e940094a0070da20d
User <user@Users-MacBook-Pro.local>
2024-02-27 13:50:49 +0100

    4 print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: prints the numbers from 0 to 9
+ *
+ * Return: 0 for success.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
